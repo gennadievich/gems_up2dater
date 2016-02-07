e4a3b21831eff4dfe43ede2862d4643dfceaf604
Kirill Kovalenko <6817821@gmail.com>
2016-02-07 14:54:11 +0300

    Rename app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_bundle_up_to_dater_session'
+Rails.application.config.session_store :cookie_store, key: '_gems_up2dater_session'
