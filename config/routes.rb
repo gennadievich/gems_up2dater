f7054c156809568de68c05c1ae5d0a0c7fa14174
Kirill Kovalenko <6817821@gmail.com>
2016-02-07 16:34:40 +0300

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root :to => "home#index"
 
   get "/home/index" => "home#index", as: :home_page
+
+  resources :users
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+    email "MyString"
+    password ""
+    role 1
+    sign_in_count 1
+  end
+end
