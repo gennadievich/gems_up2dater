484f7d114cd1cf485cc7ad2aefbb8fc9101dbaa8
Kirill Kovalenko <kkovalenko@beltechsoftware.com>
2016-04-05 18:04:58 +0300

    Working on gems controller.

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -29,13 +29,37 @@ class RubyGemsController < ApplicationController
 
   def track
     gem_name = ruby_gem_params[:name]
+    return flash.now[:warning] = "Enter gem name" unless gem_name.present?
     gem_vers = ruby_gem_params[:version]
+    return flash.now[:warning] = "Enter gem version" unless gem_vers.present?
     project  = Project.find(params[:project_id])
     ruby_gem = RubyGem.find_by(name: gem_name)
-    gem_vers = ruby_gem.gem_versions.find_by(name: gem_vers) if ruby_gem
+    gem_vers = ruby_gem.gem_versions.find_by(name: gem_vers) unless ruby_gem.nil?
 
-    if gem_vers
-      project.gem_versions << gem_vers
+    if gem_vers.is_a?(GemVersion)
+      project.gem_versions << gem_vers #if already no this gem in track list
+      flash.now[:success] = "Gem added to tracker!"
+    elsif ruby_gem
+      response = check_and_add_new_versions_for(ruby_gem)
+      flash.now[:warning] = response if response.is_a?(String)
+    else
+      gem_info = Gems.info(gem_name)
+      if gem_info.is_a?(Hash) && !gem_info.blank?
+        gem_vers = Gems.versions(ruby_gem_params[:name]).detect { |version| version['number'] == ruby_gem_params[:version] }
+        if gem_vers
+        ruby_gem = RubyGem.create(name: gem_info['name'], description: gem_info['info'],
+                                  link: gem_info['project_uri'], total_downloads: gem_info['downloads'])
+        get_all_versions_for_gem(ruby_gem)
+        ruby_gem.gem_versions.set_actual_version
+
+        project.gem_versions << ruby_gem.gem_versions.find_by(name: ruby_gem_params[:version])
+        flash.now[:success] = "Gem added to tracker!"
+        else
+          flash.now[:warning] = "Can't find this version!"
+        end
+      else
+        flash.now[:warning] = "Can't find '#{gem_name}' gem!"
+      end
     end
   end
 
@@ -88,6 +112,29 @@ class RubyGemsController < ApplicationController
                         prerelease: version['prerelease']
       )
       gem.save
+    end
+  end
+
+  def check_and_add_new_versions_for(gem)
+    local_versions_count = gem.gem_versions.count
+    versions = Gems.versions(gem.name)
+    diff = versions.count - local_versions_count
+
+    if diff == 0
+      "Please check version name"
+    elsif diff > 0
+      0.upto(diff-1) do |num| #update versions
+        gem.gem_versions.build(
+          name: versions[num]['number'],
+          date: versions[num]['built_at'],
+          this_version_downloads: versions[num]['downloads_count'],
+          actual: false,
+          prerelease: versions[num]['prerelease']
+        )
+        gem.save
+      end
+      #gem.gem_versions.reset_actual_version
+      "Gem added to tracking"
     end
   end
 end
